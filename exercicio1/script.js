71458de68e831fa6cb8865831d5a8f637de41ce3
renanpmiranda <renanpmiranda5@gmail.com>
2022-10-03 13:12:29 -0300

    a027_exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+const sapo = document.getElementById("sapo")
+sapo.innerHTML = "O sapo não lava o pé"
+
+const evidencias = document.getElementById("poema")
+evidencias.innerHTML += " evidências"
+
+const sabedoria = document.getElementById("sabedoria")
+sabedoria.innerHTML = sabedoria.innerHTML + "<span> voando<span>"
+
+const borboleta = document.getElementsByClassName("borboleta")
+borboleta[0].innerHTML += " pica-pau"
+
+const roberto = document.getElementById("roberto")
+roberto.innerHTML += " vivi"
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+let cont = 0
+
+const contaClicks = () => {
+    cont++
+    console.log(`Clicou ${cont} vezes.`)    
+}
+
